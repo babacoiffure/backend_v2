c72ff2d39a410db6fd74415b6fbd5bdee9e33bf4
Albi Ummid <albi.ummid@gmail.com>
2025-02-11 19:48:56 +0600

    update

--- src/router/routes/chat.route.ts
+++ src/router/routes/chat.route.ts
@@ -11,7 +11,7 @@ export const chatRouter = Router();
 chatRouter.get("/list", handleGetChatList);
 chatRouter.get("/message/list", handleGetChatMessageList);
 chatRouter.post("/send-message", handleSendChatMessage);
-chatRouter.post("/edit-message/:chatId/:messageId", handleSendChatMessage);
+chatRouter.patch("/edit-message/:chatId/:messageId", handleSendChatMessage);
 chatRouter.delete(
     "/delete-message/:chatId/:messageId",
     handleDeleteChatMessage
